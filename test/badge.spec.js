cd8784d2d4f7600f5c934a47159d85047ec4a209
Timber <timber@opensenselab.org>
2024-09-18 17:36:26 +0200

    Cleanup

--- test/badge.spec.js
+++ test/badge.spec.js
@@ -6,6 +6,9 @@ import {login} from './login.spec.js';
 import path from 'path';
 import {requestToken, findBadge, deleteBadge} from '../util/api.js';
 
+/**
+ * This requires that there exists a verified issuer for the user associated with the configured credentials
+ */
 async function navigateToBadgeCreation(driver) {
     await driver.get(`${url}/issuer/issuers`);
 
@@ -21,6 +24,54 @@ async function navigateToBadgeCreation(driver) {
     await driver.wait(until.titleIs('Create Badge - Open Educational Badges'), 2000);
 }
 
+/**
+ * This assumes that the driver already navigated to the badge creation page
+ */
+async function createBadge(driver) {
+    const categoryDropdownButton = await driver.findElement(By.css(
+        'button[role="combobox"]'));
+    await categoryDropdownButton.click();
+
+    const participationOption = await driver.findElement(By.css(
+        'hlm-option[ng-reflect-value="participation"]'));
+    participationOption.click();
+
+    const shortDescriptionField = await driver.findElement(By.css(
+        'textarea'));
+    await shortDescriptionField.sendKeys('automated test description');
+
+    const durationField = await driver.findElement(By.css(
+        'input[type="number"]'));
+    await durationField.sendKeys('42');
+
+    const titleField = await driver.findElement(By.css(
+        'input[type="text"]'));
+    await titleField.sendKeys('automated test title');
+
+    const imageField = await driver.findElement(By.id('image_field0'));
+    const image = path.resolve('assets/image.png');
+    await imageField.sendKeys(image);
+
+    await driver.wait(until.elementLocated(By.css(
+        'img[src^="data:image/png;base64,iVBORw0KGg"]')));
+
+    // TODO: Optionale Badge-Details
+    const submitButton = await driver.findElement(By.css(
+        'oeb-button[ng-reflect-text="Badge erstellen"]'));
+    submitButton.click();
+
+    await driver.wait(until.titleIs('Badge Class - automated test title - Open Educational Badges'), 20000);
+}
+
+async function deleteBadgeOverApi(title = 'automated test title') {
+    const apiToken = await requestToken(username, password);
+    assert(apiToken);
+    const badge = await findBadge(apiToken, title);
+    assert(badge);
+    const deletionResult = await deleteBadge(apiToken, badge.entityId);
+    assert.equal(deletionResult, true);
+}
+
 describe('Badge Test', function() {
     this.timeout(30000);
     let driver;
@@ -31,48 +82,8 @@ describe('Badge Test', function() {
     it('should create a participation badge', async function() {
         await login(driver);
         await navigateToBadgeCreation(driver);
-
-        const categoryDropdownButton = await driver.findElement(By.css(
-            'button[role="combobox"]'));
-        await categoryDropdownButton.click();
-
-        const participationOption = await driver.findElement(By.css(
-            'hlm-option[ng-reflect-value="participation"]'));
-        participationOption.click();
-
-        const shortDescriptionField = await driver.findElement(By.css(
-            'textarea'));
-        await shortDescriptionField.sendKeys('automated test description');
-
-        const durationField = await driver.findElement(By.css(
-            'input[type="number"]'));
-        await durationField.sendKeys('42');
-
-        const titleField = await driver.findElement(By.css(
-            'input[type="text"]'));
-        await titleField.sendKeys('automated test title');
-
-        const imageField = await driver.findElement(By.id('image_field0'));
-        const image = path.resolve('assets/image.png');
-        await imageField.sendKeys(image);
-
-        await driver.wait(until.elementLocated(By.css(
-            'img[src^="data:image/png;base64,iVBORw0KGg"]')));
-
-        // TODO: Optionale Badge-Details
-        const submitButton = await driver.findElement(By.css(
-            'oeb-button[ng-reflect-text="Badge erstellen"]'));
-        submitButton.click();
-
-        await driver.wait(until.titleIs('Badge Class - automated test title - Open Educational Badges'), 20000);
-
-
-        const apiToken = await requestToken(username, password);
-        assert(apiToken);
-        const badge = await findBadge(apiToken, 'automated test title');
-        assert(badge);
-        const deletionResult = await deleteBadge(apiToken, badge.entityId);
-        assert.equal(deletionResult, true);
+        await createBadge(driver);
+        await deleteBadgeOverApi();
     });
 
     after(async () => await driver.quit());
--- test/issuer.spec.js
+++ test/issuer.spec.js
@@ -6,6 +6,97 @@ import {login} from './login.spec.js';
 import path from 'path';
 import {requestToken, findIssuer, deleteIssuer} from '../util/api.js';
 
+async function navigateToIssuerCreation(driver) {
+    await driver.get(`${url}/issuer/create`);
+
+    let title = await driver.getTitle();
+    assert.equal(title, 'Create Issuer - Open Educational Badges');
+}
+
+/**
+ * This assumes that the driver already navigated to the issuer creation page
+ */
+async function createIssuer(driver) {
+    const imageField = await driver.findElement(By.id(
+        'image_field0'));
+
+    const image = path.resolve('assets/image.png');
+    await imageField.sendKeys(image);
+
+    await driver.wait(until.elementLocated(By.css(
+        'img[src^="data:image/png;base64,iVBORw0KGg"]')));
+
+    const textFields = await driver.findElements(By.css(
+        'input[type="text"]'));
+
+    const nameField = textFields[0];
+    await nameField.sendKeys('automatedTestName');
+
+    const websiteField = await driver.findElement(By.css(
+        'input[type="url"]'));
+    await websiteField.sendKeys('automatedTest.de');
+
+    const dropdownButtons = await driver.findElements(By.css(
+        'button[role="combobox"]'));
+
+    const mailDropdownButton = dropdownButtons[0];
+    await mailDropdownButton.click();
+
+    const mailOption = await driver.findElement(By.css(
+        'hlm-option[ng-reflect-value="' + username + '"]'));
+    await mailOption.click();
+
+    const description = await driver.findElement(By.tagName(
+        'textarea'));
+    await description.sendKeys('automatedTestDescription');
+
+    const categoryDropdownButton = dropdownButtons[1];
+    await categoryDropdownButton.click();
+
+    const categoryOption = await driver.findElement(By.css(
+        'hlm-option#cdk-option-2'));
+    categoryOption.click();
+
+    const streetField = textFields[1];
+    streetField.sendKeys('automatedTestStreet');
+
+    const numberFields = await driver.findElements(By.css(
+        'input[type="number"]'));
+
+    const streetnumberField = numberFields[0];
+    streetnumberField.sendKeys('42');
+
+    const postalCodeField = numberFields[1];
+    postalCodeField.sendKeys('4242');
+
+    const cityField = textFields[2];
+    cityField.sendKeys('automatedTestCity');
+
+    const altchaCheckbox = await driver.findElement(By.id(
+        'altcha_checkbox'));
+    altchaCheckbox.click();
+
+    await driver.wait(until.elementLocated(By.css(
+        'div[data-state="verified"]')), 200*1000);
+
+    const submitButton = (await driver.findElements(By.css(
+        'button[type="submit"]')))[1];
+    submitButton.click();
+
+    await driver.wait(until.titleIs('Issuer - automatedTestName - Open Educational Badges'), 2000);
+}
+
+async function deleteIssuerOverApi(name = 'automatedTestName') {
+    const apiToken = await requestToken(username, password);
+    assert(apiToken);
+    const issuer = await findIssuer(apiToken, name);
+    assert(issuer);
+    const slug = issuer.slug;
+    assert(slug);
+    const deletionResult = await deleteIssuer(apiToken, slug);
+    assert.equal(deletionResult, true);
+}
+
 describe('Issuer Test', function() {
     this.timeout(20000);
     let driver;
@@ -15,89 +106,10 @@ describe('Issuer Test', function() {
 
     it('should create an issuer', async function() {
         await login(driver);
-        await driver.get(`${url}/issuer/create`);
-
-        let title = await driver.getTitle();
-        assert.equal(title, 'Create Issuer - Open Educational Badges');
-
-        const imageField = await driver.findElement(By.id(
-            'image_field0'));
-
-        const image = path.resolve('assets/image.png');
-        await imageField.sendKeys(image);
-
-        await driver.wait(until.elementLocated(By.css(
-            'img[src^="data:image/png;base64,iVBORw0KGg"]')));
-
-        const textFields = await driver.findElements(By.css(
-            'input[type="text"]'));
-
-        const nameField = textFields[0];
-        await nameField.sendKeys('automatedTestName');
-
-        const websiteField = await driver.findElement(By.css(
-            'input[type="url"]'));
-        await websiteField.sendKeys('automatedTest.de');
-
-        const dropdownButtons = await driver.findElements(By.css(
-            'button[role="combobox"]'));
-
-        const mailDropdownButton = dropdownButtons[0];
-        await mailDropdownButton.click();
-
-        const mailOption = await driver.findElement(By.css(
-            'hlm-option[ng-reflect-value="' + username + '"]'));
-        await mailOption.click();
-
-        const description = await driver.findElement(By.tagName(
-            'textarea'));
-        await description.sendKeys('automatedTestDescription');
-
-        const categoryDropdownButton = dropdownButtons[1];
-        await categoryDropdownButton.click();
-
-        const categoryOption = await driver.findElement(By.css(
-            'hlm-option#cdk-option-2'));
-        categoryOption.click();
-
-        const streetField = textFields[1];
-        streetField.sendKeys('automatedTestStreet');
-
-        const numberFields = await driver.findElements(By.css(
-            'input[type="number"]'));
-
-        const streetnumberField = numberFields[0];
-        streetnumberField.sendKeys('42');
-
-        const postalCodeField = numberFields[1];
-        postalCodeField.sendKeys('4242');
-
-        const cityField = textFields[2];
-        cityField.sendKeys('automatedTestCity');
-
-        const altchaCheckbox = await driver.findElement(By.id(
-            'altcha_checkbox'));
-        altchaCheckbox.click();
-
-        await driver.wait(until.elementLocated(By.css(
-            'div[data-state="verified"]')), 200*1000);
-
-        const submitButton = (await driver.findElements(By.css(
-            'button[type="submit"]')))[1];
-        submitButton.click();
-
-        await driver.wait(until.titleIs('Issuer - automatedTestName - Open Educational Badges'), 2000);
-
+        await navigateToIssuerCreation(driver);
+        await createIssuer(driver);
         // TODO: Verify issuer details
-
-        const apiToken = await requestToken(username, password);
-        assert(apiToken);
-        const issuer = await findIssuer(apiToken, 'automatedTestName');
-        assert(issuer);
-        const slug = issuer.slug;
-        assert(slug);
-        const deletionResult = await deleteIssuer(apiToken, slug);
-        assert.equal(deletionResult, true);
+        await deleteIssuerOverApi();
     });
 
     after(async () => await driver.quit());
