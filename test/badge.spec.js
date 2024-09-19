8944fbfdb34587ce2869343552a7023598fb322d
Timber <timber@opensenselab.org>
2024-09-19 14:54:18 +0200

    Download received PDF

--- test/badge.spec.js
+++ test/badge.spec.js
@@ -1,9 +1,11 @@
 import {By, Builder, Browser, until, WebElementCondition} from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
 import assert from 'assert';
 import {username, password} from '../secret.js';
 import {url} from '../config.js';
 import {login} from './login.spec.js';
 import path from 'path';
+import fs from 'fs';
 import {requestToken, findBadge, deleteBadge, findAssertions, revokeAssertions} from '../util/api.js';
 
 /**
@@ -61,8 +63,18 @@ async function navigateToBadgeAwarding(driver, name = 'automated test title') {
 async function navigateToBackpack(driver) {
     await driver.get(`${url}/recipient/badges`);
 
-    let title = await driver.getTitle();
-    assert.equal(title, 'Backpack - Open Educational Badges');
+    const title = await driver.getTitle();
+    driver.wait(until.titleIs('Backpack - Open Educational Badges'), 2000);
+}
+
+async function navigateToReceivedBadge(driver, badgeName = 'automated test title') {
+    await navigateToBackpack(driver);
+    const receivedBadgeCard = await driver.findElement(By.css(
+        `bg-badgecard[ng-reflect-badge-title="${badgeName}"]`));
+    const receivedBadgeLink = await receivedBadgeCard.findElement(By.tagName('a'));
+    receivedBadgeLink.click();
+
+    await driver.wait(until.titleIs(`Backpack - ${badgeName} - Open Educational Badges`), 2000);
 }
 
 /**
@@ -136,6 +148,49 @@ async function receiveBadge(driver, badgeName = 'automated test title') {
 }
 
 /**
+ * This assumes that the driver already navigated to the received badge page
+ */
+async function downloadPdfFromBackpack(driver, badgeName = 'automated test title') {
+    const moreSvgButton = await driver.findElement(By.css(
+        'svg[icon="icon_more"]'));
+    await moreSvgButton.click();
+
+    const dropdownButtons = await driver.findElements(By.css(
+        'button[role="menuitem"]'));
+    const pdfExportButton = dropdownButtons[1];
+    await pdfExportButton.click();
+
+    await driver.wait(until.elementLocated(By.css('embed[src^="blob:http"]')));
+    const htmlEmbed = await driver.findElement(By.css('embed[src^="blob:http"]'));
+    await driver.switchTo().frame(htmlEmbed);
+
+    await driver.wait(until.elementLocated(By.css('embed[src="about:blank"]')));
+
+    await driver.switchTo().defaultContent();
+    const downloadButton = await driver.findElement(By.css(
+        'button[type="submit"]'));
+    await downloadButton.click();
+
+    const downloadPoll = resolve => {
+        const files = fs.readdirSync('./download');
+        if (files.length == 0) {
+            setTimeout(_ => downloadPoll(resolve), 100);
+            return;
+        }
+        assert(files.length, 1, "Expected one downloaded file");
+        const regex = new RegExp(`${badgeName} - \\d+\\.pdf`);
+        if (!regex.test(files[0])) {
+            setTimeout(_ => downloadPoll(resolve), 100);
+            return;
+        }
+        resolve();
+    };
+    const pollingPromise = new Promise(downloadPoll);
+    await driver.wait(pollingPromise, 20000, "Download didn't finish or file content didn't match the pattern within the specified timeout");
+    // TODO: Verify file content
+}
+
+/**
  * This assumes that the driver already navigated to the badge detail page
  */
 async function revokeBadge(driver, badgeName = 'automated test title') {
@@ -176,7 +231,20 @@ describe('Badge Test', function() {
     this.timeout(30000);
     let driver;
 
-    before(async () => driver = await new Builder().forBrowser(Browser.CHROME).build());
+    before(async () => {
+        if (!fs.existsSync('./download')){
+            fs.mkdirSync('./download');
+        }
+        const downloadPath = path.resolve('download');
+        let options = new chrome.Options();
+        options.setUserPreferences({
+            "download.default_directory": downloadPath,
+        });
+        driver = await new Builder()
+            .forBrowser(Browser.CHROME)
+            .setChromeOptions(options)
+            .build()
+    });
 
     // This requires that there exists a verified issuer for the user associated with the configured credentials
     it('should create a participation badge', async function() {
@@ -195,6 +263,11 @@ describe('Badge Test', function() {
         await receiveBadge(driver);
     });
 
+    it('should download the pdf from the backpack', async function() {
+        await navigateToReceivedBadge(driver);
+        await downloadPdfFromBackpack(driver);
+    });
+
     it('should revoke the badge', async function() {
         await navigateToBadgeDetails(driver);
         await revokeBadge(driver);
@@ -205,5 +278,6 @@ describe('Badge Test', function() {
     after(async () => {
         await deleteBadgeOverApi();
         await driver.quit();
+        fs.rmSync('./download', { recursive: true, force: true });
     });
 });
