9d411ab9ebeceee4d4e5e36ec8609d6c0ae2ca2d
YouQam <qamazyousef@gmail.com>
2024-12-12 08:48:35 +0100

    increase waiting time

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 //export const backendUrl = "http://localhost:8000";
 export const url = "https://staging.openbadges.education";
 export const backendUrl = "https://api.staging.openbadges.education";
-export const defaultWait = 8000;    // waits for a specified amount of time for a certain condition to be true. 
+export const defaultWait = 12000;    // waits for a specified amount of time for a certain condition to be true. 
 export const implicitWait = 2000;   // waits for a specified amount of time before throwing a no such element exception. 
-export const extendedWait = 18000; 
+export const extendedWait = 25000; 
