6f0ca767fa25ee6f8ca06fa691076fb6bf0a57ea
Timber <timber@opensenselab.org>
2025-01-15 15:42:27 +0100

    Repeat to avoid stale elements

--- util/selection.js
+++ util/selection.js
@@ -3,34 +3,44 @@ import {By, WebElement} from 'selenium-webdriver';
 export class ExtendedBy {
     static containingText(selector, childSelector, text, trim = true) {
         return (async (driver) => {
-            // Find elements from outer selector
-            const selected = await driver.findElements(selector);
-            const selectedArray = Array.from(selected);
-            // Filter the elements.
-            // Since the filtering is done async, first map them
-            const mappedPromises = await selectedArray.map(async node => {
-                // Find the children from the child selector
-                const children = await node.findElements(childSelector);
-                const childrenArray = Array.from(children);
-                // Find if one or more children match the text.
-                // Since this is done asnyc, first map them
-                const mappedPromises = childrenArray.map(async node => {
-                    let nodeText = await node.getText();
-                    if (trim) {
-                        nodeText = nodeText.trim();
-                        text = text.trim();
-                    }
-                    return nodeText == text;
-                });
-                // Wait for the results of the mapping
-                const mapped = await Promise.all(mappedPromises);
-                // Check if at least one element matched
-                return mapped.some(_ => _);
-            });
-            // Wait for the results of the mapping
-            const mapped = await Promise.all(mappedPromises);
-            // Filter the elements where the mapping yielded true
-            return selectedArray.filter((_, i) => mapped[i]);
+            // Try 5 times to avoid stale reference errors
+            for (let i = 0; i < 5; i++) {
+                try {
+                    // Find elements from outer selector
+                    const selected = await driver.findElements(selector);
+                    const selectedArray = Array.from(selected);
+                    // Filter the elements.
+                    // Since the filtering is done async, first map them
+                    const mappedPromises = await selectedArray.map(async node => {
+                        // Find the children from the child selector
+                        const children = await node.findElements(childSelector);
+                        const childrenArray = Array.from(children);
+                        // Find if one or more children match the text.
+                        // Since this is done asnyc, first map them
+                        const mappedPromises = childrenArray.map(async node => {
+                            let nodeText = await node.getText();
+                            if (trim) {
+                                nodeText = nodeText.trim();
+                                text = text.trim();
+                            }
+                            return nodeText == text;
+                        });
+                        // Wait for the results of the mapping
+                        const mapped = await Promise.all(mappedPromises);
+                        // Check if at least one element matched
+                        return mapped.some(_ => _);
+                    });
+                    // Wait for the results of the mapping
+                    const mapped = await Promise.all(mappedPromises);
+                    // Filter the elements where the mapping yielded true
+                    return selectedArray.filter((_, i) => mapped[i]);
+                } catch(e) {
+                    if (e.name === 'StaleElementReferenceError')
+                        continue;
+                    throw e;
+                }
+                break;
+            }
         });
     }
 
