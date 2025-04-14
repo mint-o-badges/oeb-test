445cbab384e7b6856404631973ef74e0960e206f
David Scheidt <david.scheidt@opensenselab.org>
2025-04-14 13:29:35 +0200

    refactor: revert to develop for running the tests

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const url = "https://staging.openbadges.education";
-export const backendUrl = "https://api.staging.openbadges.education";
+export const url = "https://develop.openbadges.education";
+export const backendUrl = "https://api.develop.openbadges.education";
 export const defaultWait = 12000;    // waits for a specified amount of time for a certain condition to be true. 
 export const implicitWait = 2000;   // waits for a specified amount of time before throwing a no such element exception. 
 export const extendedWait = 25000; 
