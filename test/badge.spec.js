8e53cb79bf916b6e61413d7f1c782941c15fff78
David Scheidt <david.scheidt@opensenselab.org>
2025-04-16 11:10:09 +0200

    refactor: uncomment all tests again

--- test/badge.spec.js
+++ test/badge.spec.js
@@ -62,64 +62,64 @@ describe('Badge Test', function() {
         await login(driver);
     });
 
-    // // Participation badge
-    // it('should create a participation badge', async function() {
-    //     await navigateToBadgeCreation(driver);
-    //     await createBadge(driver);
-    // });
+    // Participation badge
+    it('should create a participation badge', async function() {
+        await navigateToBadgeCreation(driver);
+        await createBadge(driver);
+    });
 
-    // it('should validate the participation badge', async function() {
-    //     await navigateToBadgeDetails(driver);
-    //     await validateBadge(driver);
-    //     await verifyBadgeOverApi(driver);
-    // });
+    it('should validate the participation badge', async function() {
+        await navigateToBadgeDetails(driver);
+        await validateBadge(driver);
+        await verifyBadgeOverApi(driver);
+    });
 
-    // it('delete the participation badge', async function() {
-    //     await deleteBadgeOverApi();
-    // });
+    it('delete the participation badge', async function() {
+        await deleteBadgeOverApi();
+    });
 
-    // // Competency badge
-    // it('should create a competency badge', async function() {
-    //     await navigateToBadgeCreation(driver);
-    //     await createBadge(driver, 'competency');
-    // });
+    // Competency badge
+    it('should create a competency badge', async function() {
+        await navigateToBadgeCreation(driver);
+        await createBadge(driver, 'competency');
+    });
 
-    // it('should validate the competency badge', async function() {
-    //     await navigateToBadgeDetails(driver);
-    //     await validateBadge(driver, 'Kompetenz');
-    //     await verifyBadgeOverApi(driver);
-    // });
+    it('should validate the competency badge', async function() {
+        await navigateToBadgeDetails(driver);
+        await validateBadge(driver, 'Kompetenz');
+        await verifyBadgeOverApi(driver);
+    });
 
-    // it('should award the competency badge', async function() {
-    //     await navigateToBadgeAwarding(driver);
-    //     await awardBadge(driver);
-    // });
+    it('should award the competency badge', async function() {
+        await navigateToBadgeAwarding(driver);
+        await awardBadge(driver);
+    });
 
-    // it('should receive the competency badge', async function() {
-    //     await navigateToBackpack(driver);
-    //     await receiveBadge(driver);
-    // });
+    it('should receive the competency badge', async function() {
+        await navigateToBackpack(driver);
+        await receiveBadge(driver);
+    });
 
-    // it('should download the pdf from the backpack', async function() {
-    //     await navigateToReceivedBadge(driver);
-    //     await downloadPdfFromBackpack(driver);
-    // });
+    it('should download the pdf from the backpack', async function() {
+        await navigateToReceivedBadge(driver);
+        await downloadPdfFromBackpack(driver);
+    });
 
-    // it('should download the pdf from the internal issuer page', async function() {
-    //     await navigateToBadgeDetails(driver);
-    //     await downloadPdfFromIssuer(driver);
-    // });
+    it('should download the pdf from the internal issuer page', async function() {
+        await navigateToBadgeDetails(driver);
+        await downloadPdfFromIssuer(driver);
+    });
 
-    // it('should revoke the competency badge', async function() {
-    //     await navigateToBadgeDetails(driver);
-    //     await revokeBadge(driver);
-    //     await navigateToBackpack(driver);
-    //     await confirmRevokedBadge(driver);
-    // });
+    it('should revoke the competency badge', async function() {
+        await navigateToBadgeDetails(driver);
+        await revokeBadge(driver);
+        await navigateToBackpack(driver);
+        await confirmRevokedBadge(driver);
+    });
 
-    // it('delete the competency badge', async function() {
-    //     await deleteBadgeOverApi();
-    // })
+    it('delete the competency badge', async function() {
+        await deleteBadgeOverApi();
+    })
 
     // micro degree
     it('should create a micro degree', async () => {
